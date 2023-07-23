f7338ddae5ecb3fcea41bfa895f39bb0e75292b4
Nilay Nagar <45895008+nilzzzz19@users.noreply.github.com>
2023-07-23 00:09:00 -0700

    node backend added

---
+++ story-builder/server/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const { Configuration, OpenAIApi } = require('openai');
+
+const configuration = new Configuration({
+    apiKey: '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',
+});
+const openai = new OpenAIApi(configuration);
+const history = [];
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/generate-text', async (req, res) => {
+  const user_input = req.body.prompt;
+
+  const messages = [];
+  for (const [input_text, completion_text] of history) {
+    messages.push({ role: "user", content: input_text });
+    messages.push({ role: "assistant", content: completion_text });
+  }
+
+  messages.push({ role: "user", content: user_input });
+
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: messages,
+    });
+
+    const completion_text = completion.data.choices[0].message.content;
+    history.push([user_input, completion_text]);
+
+    res.json(completion_text);
+
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+      res.status(500).send('Error generating text');
+    } else {
+      console.log(error.message);
+      res.status(500).send('Error generating text');
+    }
+  }
+});
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
--- story-builder/src/App.js
+++ story-builder/src/App.js
@@ -1,23 +1,32 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 import StoryInput from './components/StoryInput';
 import StoryDisplay from './components/StoryDisplay';
 import Navbar from './components/Navbar';
 
 function App() {
-    const [story, setStory] = useState('');
+  const [story, setStory] = useState('');
 
-    const handleStoryStart = (storyStart) => {
-        setStory(storyStart + ' and they lived happily ever after.');
-    };
+  const handleStoryStart = async (storyStart) => {
+    try {
+      const res = await axios.post('http://localhost:5000/api/generate-text', { prompt: storyStart });
+      setStory(res.data);
+      //console.log(res?.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-    return (
-        <div>
-          <Navbar />
-            <StoryInput onStoryStart={handleStoryStart} />
-            <StoryDisplay story={story} />
-        </div>
-    );
+  console.log(story);
+  
+  return (
+    <div>
+      <Navbar />
+      <StoryInput onStoryStart={handleStoryStart} />
+      <StoryDisplay story={story} />
+    </div>
+  );
 }
 
 export default App;
